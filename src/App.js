657c7b0b21ad98c61d4c7f34ad06a7c21a113c9a
ZacharyBrenk <zachbrenk@gmail.com>
2025-05-22 18:32:13 -0500

    this is a save

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import React, { useRef } from "react";
-
-const ContactForm = () => {
-  const formRef = useRef(null);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch("https://script.google.com/macros/s/AKfycbxB7wWk5L_B2iEiLDSQri1HpCP8WXF5K1pWB3eEN5ag08r1lNQk9UlRcovzwHVIIGVC/exec", {
-      method: 'POST',
-      body: new FormData(formRef.current),
-    }).then(res => res.json())
-      .then(data => {
-        console.log(data);
-        alert(data.msg);
-      })
-      .catch(err => console.log(err));
-  };
+import React from 'react'
+import './App.css';
+import { QRCode } from 'react-qrcode-logo';
+const App = () => {
   return (
-
-    
-    <Container style={{ width: "50%", height: "100vh", alignItems: "center", display: "flex", justifyContent: "center" }}>
-      <Form style={{ textAlign: "left", backgroundColor: "gray", padding: "5%", borderRadius: "15px" }} ref={formRef} onSubmit={handleSubmit}>
-        <h3>Contact Form</h3>
-        <Form.Group className="mb-3" controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" name="Name" required />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email Id</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" name="Email" required />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="Description">
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} name="Message" required />
-        </Form.Group>
-        <Button style={{ float: "right" }} variant="success" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+    <>
+      <h1>
+      Autonomous
+      </h1>
+      
+    <input type="counter" id ="counter"></input>
+    <label htmlFor ="counter">L4</label>
+    <div>
+        <input type="checkbox" id ="checkbox"></input>
+        <label htmlFor ="checkbox">Leave Start?</label>
+    </div>
+    <div>
+      <input type="text" id ="text"></input>
+      <label htmlFor ="text">Comments</label>
+    </div>
+    <QRCode value="this is data"/>
+    <div>
+    </>
   );
-};
-export default ContactForm;
+}
+
+export default App;
