cf4133ccf93d264d5780ef774d4139b26b51221f
ZacharyBrenk <zachbrenk@gmail.com>
2025-05-29 20:05:22 -0500

    final commits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import QRCode from 'react-qr-code';
+import './App.css';
 
 function App() {
   const [text, setText] = useState('');
   const [checkBox, setCheckBox] = useState(false);
   const [counter, setCounter] = useState(0)
+
+  const handleIncrement = () => setCounter((prev) => prev + 1);
+  const handleDecrement = () => setCounter((prev) => prev - 1);
 
   const qrValue = `${text} \t ${checkBox} \t ${counter}`;
   return (
@@ -12,21 +16,32 @@ function App() {
       <h1>
         Autonomous
       </h1>
+      <div>
       <input
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
-      />      
+      />
+      </div>
+      <div>
+        <label>
       <input
         type="checkBox"
         checked={checkBox}
         onChange={(e) => setCheckBox(e.target.checked)}
       />
+      Include option
+      </label>
+      <div>
+        <button onClick={handleDecrement} disabled={counter <= 0}>-</button>
         <input
         type="number"
         value={counter}
         onChange={(e) => setCounter(Number(e.target.value))}
-    />
+        />
+        <button onClick={handleIncrement}>+</button>
+      </div>
+      </div>
       <QRCode value={qrValue}/>
     </div>
   );
